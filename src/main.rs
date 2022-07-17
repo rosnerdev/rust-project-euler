30be298fa4632cf760ff253102b858ebb9abcdba
rosnerdev <yrosner123@gmail.com>
2022-07-17 22:43:09 +0300

    Update main.rs
    Added comments for beginners

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// I didn't want to make the main file to messy so I created separate files to contain the functions that solve the problems presented by Project Euler
+// What the code below does is to import the two files as modules which we can extract functions from by using two colons or ::
 mod euler1;
 mod euler2;
 
@@ -5,3 +7,5 @@ fn main() {
     euler1::print_sum_of_multiples();
     euler2::print_sum_of_sequence();
 }
+
+// Run using the 'cargo run' command
